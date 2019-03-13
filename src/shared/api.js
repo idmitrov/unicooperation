9d43ef9898b3b2274bbdf08e5bd4a86243db65f7
Dimitar <mailto.dimitrov@gmail.com>
2019-03-13 23:08:43 +0200

    Init Api middleware

---
+++ src/shared/api.js
@@ -0,0 +1,61 @@
+import appConfig from '../app/App.config';
+
+export default (store) => (next) => (action) => {
+    if (action.api) {
+        const state = store.getState();
+        let options = {
+            method: action.api.method,
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            }
+        };
+
+        if ((options.method.test(/^POST$|^PUT$|^PATCH$/)) && action.payload) {
+            options.body = JSON.stringify(action.payload);
+        }
+
+        if (action.api.query) {
+            action.api.endpoint += `?${action.api.query}`;
+        }
+
+        if (state.user.account.isLoggedin) {
+            options.headers['Authorization'] = `Bearer ${state.user.account.token}`;
+        }
+
+        return new Promise((resolve, reject) => {
+            fetch(`//${appConfig.REACT_APP_API_URL}/${action.api.endpoint}`, options)
+                .then((response) => {
+                    if (/application\/json/.test(response.headers.get('Content-Type'))) {
+                        return response.json();
+                    }
+
+                    return response.text();
+                })
+                .then((response) => {
+                    if (response.errors.length) {
+                        response.errors.forEach((error) => {
+                            console.error(error);
+                        });
+
+                        reject(response.errors);
+                    } else {
+                        if (action.api.successMessage) {
+                            console.log(action.api.successMessage);
+                        }
+
+                        resolve(response.data);
+                    }
+                })
+                .catch((response) => {
+                    console.error('Error', 'Something went wrong');
+
+                    if (appConfig.NODE_ENV !== 'production') {
+                        console.error(response);
+                    }
+                })
+        });
+    }
+
+    next(action);
+}
--- src/shared/store.js
+++ src/shared/store.js
@@ -1,24 +1,30 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import { connect } from 'react-redux';
+import { connect as __connect__ } from 'react-redux';
 import thunk from 'redux-thunk';
 
 import appReducer from '../app/App.reducer';
+import feedReducer from '../feed/Feed.reducer';
+
+import api from '../shared/api';
 
 const store = createStore(
     combineReducers({
-        app: appReducer
+        app: appReducer,
+        feed: feedReducer
     }),
     composeWithDevTools(
         applyMiddleware(
-            thunk
+            thunk,
+            api
         )
     )
 );
 
 export const mapStateToProps = (mapStateToProps) => connect(mapStateToProps, {});
 export const mapDispatchToProps = (mapDispatchToProps) => connect({}, mapDispatchToProps);
+export const connect = __connect__;
 
 export default store;
 
