c786b4de741dd41105cca7d540d53306e6901923
Dimitar <mailto.dimitrov@gmail.com>
2019-03-14 00:30:17 +0200

    Init header

--- src/app/App.js
+++ src/app/App.js
@@ -5,14 +5,29 @@ import Router, { Routes } from '../shared/router';
 import store from '../shared/store';
 import history from '../shared/history';
 
+import './App.scss';
+import logo from './Logo.svg';
+
 class Main extends Component {
     render() {
         return (
             <Provider store={store}>
                 <Router history={history}>
                     <Fragment>
-                        <header className="container">
-                            Unicooperation
+                        <header className="container-fluid">
+                            <div className="row">
+                                <div className="col">
+                                    <a href="/" title="Unicooperation">
+                                        <img className="logo" src={logo} />
+                                    </a>
+
+                                    Home
+                                </div>
+
+                                <nav className="col-auto">
+                                    RIGHT NAV
+                                </nav>
+                            </div>
                         </header>
 
                         <main>
