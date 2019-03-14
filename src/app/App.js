55e65f5b4c3b1587a24f785568c51d87b66c6a3e
Dimitar <mailto.dimitrov@gmail.com>
2019-03-14 02:07:15 +0200

    Add mdl icons and tweak header

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,18 @@
 import React, { Component, Fragment } from 'react';
 import { Provider } from 'react-redux';
 
+import Home from 'mdi-react/HomeIcon';
+import UserCircle from 'mdi-react/UserCircleIcon';
+import Magnify from 'mdi-react/MagnifyIcon';
+import Power from 'mdi-react/PowerIcon';
+
 import Router, { Routes } from '../shared/router';
 import store from '../shared/store';
 import history from '../shared/history';
 
 import './App.scss';
 import logo from './Logo.svg';
+import { Link } from '../shared/router';
 
 class Main extends Component {
     render() {
@@ -14,18 +20,30 @@ class Main extends Component {
             <Provider store={store}>
                 <Router history={history}>
                     <Fragment>
-                        <header className="container-fluid">
-                            <div className="row">
+                        <header>
+                            <div className="row align-items-center">
                                 <div className="col">
                                     <a href="/" title="Unicooperation">
-                                        <img className="logo" src={logo} />
+                                        <img className="logo" src={logo} alt="unicooperation-logo"/>
                                     </a>
 
-                                    Home
+                                    <Link className="header-button" to="/" title="Home">
+                                        <Home size={32} />
+                                    </Link>
                                 </div>
 
                                 <nav className="col-auto">
-                                    RIGHT NAV
+                                    <button className="header-button" title="Search">
+                                        <Magnify size={32} />
+                                    </button>
+
+                                    <Link className="header-button" to="/user/profile" title="Profile">
+                                        <UserCircle size={32} />
+                                    </Link>
+
+                                    <button className="header-button header-button-primary" title="Logout">
+                                        <Power size={32} />
+                                    </button>
                                 </nav>
                             </div>
                         </header>
--- src/shared/router.js
+++ src/shared/router.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch, Link as _Link } from 'react-router-dom';
 
 import Feed from '../feed/Feed.view';
+
+export const Link = _Link;
 
 export const Routes = () => {
     return (
