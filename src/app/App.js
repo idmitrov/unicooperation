19988353323c2c8c13fc0449450edcd9523e59c2
Dimitar <mailto.dimitrov@gmail.com>
2019-03-14 23:49:03 +0200

    Implement PrivateROute

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
-import React, { Component, Fragment } from 'react';
-import { Provider } from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Home from 'mdi-react/HomeIcon';
 import UserCircle from 'mdi-react/UserCircleIcon';
@@ -8,71 +8,91 @@ import Power from 'mdi-react/PowerIcon';
 import Menu from 'mdi-react/MenuIcon';
 
 import Router, { Routes } from '../shared/router';
-import store from '../shared/store';
 import history from '../shared/history';
 
 import './App.scss';
 import logo from './Logo.svg';
 import { Link } from '../shared/router';
 
-class Main extends Component {
+class App extends Component {
     render() {
+        const { authenticated } = this.props;
+
         return (
-            <Provider store={store}>
-                <Router history={history}>
-                    <Fragment>
-                        <header>
-                            <div className="row align-items-center no-gutters">
-                                <div className="col">
-                                    <div className="row no-gutters justify-content-between">
-                                        <div className="col-auto">
-                                            <Link className="header-button" to="/" title="Home">
-                                                <Home size={32} />
-                                            </Link>
+            <Router history={history}>
+                <React.Fragment>
+                    {
+                        authenticated ? (
+                            <header>
+                                <div className="row align-items-center no-gutters">
+                                    <div className="col">
+                                        <div className="row no-gutters justify-content-between">
+                                            <div className="col-auto">
+                                                <Link className="header-button" to="/" title="Home">
+                                                    <Home size={32} />
+                                                </Link>
+                                            </div>
+                                            <div className="col-auto d-md-none">
+                                                <button className="header-button header-button-primary">
+                                                    <Menu size={32} />
+                                                </button>
+                                            </div>
                                         </div>
-                                        <div className="col-auto d-md-none">
-                                            <button className="header-button header-button-primary">
-                                                <Menu size={32} />
-                                            </button>
-                                        </div>
+
+                                        <a href="/" title="Unicooperation">
+                                            <img className="logo" src={logo} alt="unicooperation-logo" />
+                                        </a>
                                     </div>
 
-                                    <a href="/" title="Unicooperation">
-                                        <img className="logo" src={logo} alt="unicooperation-logo" />
-                                    </a>
+                                    {/* NAV */}
+                                    <nav id="drawer" className="col-auto">
+                                        <ul className="row flex-column flex-md-row no-gutters">
+                                            <li className="col-auto">
+                                                <button className="header-button" title="Search">
+                                                    <Magnify size={32} />
+                                                </button>
+                                            </li>
+                                            <li className="col-auto">
+                                                <Link className="header-button" to="/user/profile" title="Profile">
+                                                    <UserCircle size={32} />
+                                                </Link>
+                                            </li>
+                                            <li className="col-auto">
+                                                <button className="header-button header-button-primary" title="Logout">
+                                                    <Power size={32} />
+                                                </button>
+                                            </li>
+                                        </ul>
+                                    </nav>
                                 </div>
+                            </header>
+                        ) : (null)
+                    }
 
-                                {/* NAV */}
-                                <nav id="drawer" className="col-auto">
-                                    <ul className="row flex-column flex-md-row no-gutters">
-                                        <li className="col-auto">
-                                            <button className="header-button" title="Search">
-                                                <Magnify size={32} />
-                                            </button>
-                                        </li>
-                                        <li className="col-auto">
-                                            <Link className="header-button" to="/user/profile" title="Profile">
-                                                <UserCircle size={32} />
-                                            </Link>
-                                        </li>
-                                        <li className="col-auto">
-                                            <button className="header-button header-button-primary" title="Logout">
-                                                <Power size={32} />
-                                            </button>
-                                        </li>
-                                    </ul>
-                                </nav>
-                            </div>
-                        </header>
-
-                        <main>
-                            <Routes />
-                        </main>
-                    </Fragment>
-                </Router>
-            </Provider>
+                    <main>
+                        <Routes authenticated={authenticated} />
+                    </main>
+                </React.Fragment>
+            </Router>
         );
     }
 }
 
-export default Main;
+const mapStateToProps = (state) => {
+    return {
+        authenticated: state.account.authenticated
+    };
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        login() {
+
+        },
+        register() {
+
+        }
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/shared/router.js
+++ src/shared/router.js
@@ -2,13 +2,29 @@ import React from 'react';
 import { Router, Route, Switch, Link as _Link } from 'react-router-dom';
 
 import Feed from '../feed/Feed.view';
+import Welcome from '../welcome/Welcome.view';
 
 export const Link = _Link;
 
-export const Routes = () => {
+const PrivateRoute = ({ component: Component, fallbackComponent: FallBackComponent, authenticated, ...rest }) => {
+    return (
+        <Route
+            {...rest}
+            render={
+                (props) => authenticated ? (
+                    <Component {...rest} {...props} />
+                ) : (
+                    <FallBackComponent {...rest} {...props} />
+                )
+            }
+        />
+    );
+}
+
+export const Routes = ({ authenticated }) => {
     return (
         <Switch>
-            <Route path="/"  component={Feed} exact={true} />
+            <PrivateRoute path="/" component={Feed} fallbackComponent={Welcome} authenticated={authenticated} exact={true} />
             <Route path="*" render={() => <div>The page was not found</div>} />
         </Switch>
     );
