0e272c510a0491cfc1b44d555341d6d861c40b5d
Dimitar <mailto.dimitrov@gmail.com>
2019-03-14 16:54:03 +0200

    Init mobile drawer

--- src/app/App.js
+++ src/app/App.js
@@ -5,6 +5,7 @@ import Home from 'mdi-react/HomeIcon';
 import UserCircle from 'mdi-react/UserCircleIcon';
 import Magnify from 'mdi-react/MagnifyIcon';
 import Power from 'mdi-react/PowerIcon';
+import Menu from 'mdi-react/MenuIcon';
 
 import Router, { Routes } from '../shared/router';
 import store from '../shared/store';
@@ -21,29 +22,45 @@ class Main extends Component {
                 <Router history={history}>
                     <Fragment>
                         <header>
-                            <div className="row align-items-center">
+                            <div className="row align-items-center no-gutters">
                                 <div className="col">
-                                    <a href="/" title="Unicooperation">
-                                        <img className="logo" src={logo} alt="unicooperation-logo"/>
-                                    </a>
+                                    <div className="row no-gutters justify-content-between">
+                                        <div className="col-auto">
+                                            <Link className="header-button" to="/" title="Home">
+                                                <Home size={32} />
+                                            </Link>
+                                        </div>
+                                        <div className="col-auto d-md-none">
+                                            <button className="header-button header-button-primary">
+                                                <Menu size={32} />
+                                            </button>
+                                        </div>
+                                    </div>
 
-                                    <Link className="header-button" to="/" title="Home">
-                                        <Home size={32} />
-                                    </Link>
+                                    <a href="/" title="Unicooperation">
+                                        <img className="logo" src={logo} alt="unicooperation-logo" />
+                                    </a>
                                 </div>
 
-                                <nav className="col-auto">
-                                    <button className="header-button" title="Search">
-                                        <Magnify size={32} />
-                                    </button>
-
-                                    <Link className="header-button" to="/user/profile" title="Profile">
-                                        <UserCircle size={32} />
-                                    </Link>
-
-                                    <button className="header-button header-button-primary" title="Logout">
-                                        <Power size={32} />
-                                    </button>
+                                {/* NAV */}
+                                <nav id="drawer" className="col-auto">
+                                    <ul className="row flex-column flex-md-row no-gutters">
+                                        <li className="col-auto">
+                                            <button className="header-button" title="Search">
+                                                <Magnify size={32} />
+                                            </button>
+                                        </li>
+                                        <li className="col-auto">
+                                            <Link className="header-button" to="/user/profile" title="Profile">
+                                                <UserCircle size={32} />
+                                            </Link>
+                                        </li>
+                                        <li className="col-auto">
+                                            <button className="header-button header-button-primary" title="Logout">
+                                                <Power size={32} />
+                                            </button>
+                                        </li>
+                                    </ul>
                                 </nav>
                             </div>
                         </header>
