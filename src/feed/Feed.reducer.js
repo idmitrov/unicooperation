d89bf50cf2c4bd956b8a193a4627c96141dcc43c
Dimitar <mailto.dimitrov@gmail.com>
2019-03-18 23:12:13 +0200

    Init feed

--- src/feed/Feed.reducer.js
+++ src/feed/Feed.reducer.js
@@ -1,5 +1,51 @@
 const feedDefaults = {
-    data: []
+    data: [
+        {
+            content: 'Test post 1',
+            date: '01/01/2019:14:00',
+            owner: 'John Doe'
+        },
+        {
+            content: 'Test post 2',
+            date: '01/01/2019:14:53',
+            owner: 'John Doe'
+        },
+        {
+            content: 'Test post 3',
+            date: '01/01/2019:14:56',
+            owner: 'John Doe'
+        },
+        {
+            content: 'Test post 1',
+            date: '01/01/2019:14:00',
+            owner: 'John Doe'
+        },
+        {
+            content: 'Test post 2',
+            date: '01/01/2019:14:53',
+            owner: 'John Doe'
+        },
+        {
+            content: 'Test post 3',
+            date: '01/01/2019:14:56',
+            owner: 'John Doe'
+        },
+        {
+            content: 'Test post 1',
+            date: '01/01/2019:14:00',
+            owner: 'John Doe'
+        },
+        {
+            content: 'Test post 2',
+            date: '01/01/2019:14:53',
+            owner: 'John Doe'
+        },
+        {
+            content: 'Test post 3',
+            date: '01/01/2019:14:56',
+            owner: 'John Doe'
+        }
+    ]
 };
 
 export default (state = feedDefaults, action) => {
