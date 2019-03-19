56089186bb8ee102c6375d8cec0393199912ac6a
Dimitar <mailto.dimitrov@gmail.com>
2019-03-19 21:52:15 +0200

    Add avatar

--- src/account/Account.reducer.js
+++ src/account/Account.reducer.js
@@ -5,13 +5,15 @@ const __cachedAccount = JSON.parse(localStorage.getItem('uniaccount'));
 const accountReset = {
     authenticated: false,
     token: null,
-    email: null
+    email: null,
+    avatar: null
 };
 
 const accountDefaults = {
     authenticated: __cachedAccount !== null,
     token: __cachedAccount ? __cachedAccount.token : accountReset.token,
-    email: __cachedAccount ? __cachedAccount.email : accountReset.email
+    email: __cachedAccount ? __cachedAccount.email : accountReset.email,
+    avatar: __cachedAccount ? __cachedAccount.avatar : accountReset.avatar,
 };
 
 export default (state = accountDefaults, action) => {
