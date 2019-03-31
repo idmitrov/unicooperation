18d1d30e590f2eb48b0d4cfc41afe77971bb16fe
Dimitar <mailto.dimitrov@gmail.com>
2019-03-31 02:52:36 +0200

    Init UniversitySetupView

---
+++ src/setup/UniversitySetup.view.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UniversitySetupView extends Component {
+    render() {
+        return (
+            <div>
+                UNI SETUP
+            </div>
+        )
+    }
+}
+
+export default UniversitySetupView;
