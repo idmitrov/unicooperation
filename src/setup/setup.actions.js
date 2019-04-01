757d2195f2a6b416bd85daf7dc345444901c0e11
Dimitar <mailto.dimitrov@gmail.com>
2019-04-01 20:16:57 +0300

    add createPartnerSetup

--- src/setup/setup.actions.js
+++ src/setup/setup.actions.js
@@ -1,7 +1,8 @@
 import setupEndpoints from './Setup.endpoints';
 
 export const setupActionTypes = {
-    createUniversitySetup: 'SETUP_UNIVERSITY_CREATE'
+    createUniversitySetup: 'SETUP_UNIVERSITY_CREATE',
+    createPartnerSetup: 'SETUP_PARTNER_CREATE'
 };
 
 export const createUniversitySetup = (name, countryCode) => (dispatch) => {
@@ -11,6 +12,17 @@ export const createUniversitySetup = (name, countryCode) => (dispatch) => {
         api: {
             endpoint: setupEndpoints.createUniversitySetup.endpoint,
             method: setupEndpoints.createUniversitySetup.method
+        }
+    });
+}
+
+export const createPartnerSetup = (name, countryCode) => (dispatch) => {
+    return dispatch({
+        type: setupActionTypes.createPartnerSetup,
+        payload: { name, countryCode },
+        api: {
+            endpoint: setupEndpoints.createPartnerSetup.endpoint,
+            method: setupEndpoints.createPartnerSetup.method
         }
     });
 }
