eafe5f3b97dc40982d93b7d98e8b6f822e5c182e
Dimitar <mailto.dimitrov@gmail.com>
2019-04-07 00:38:07 +0300

    Init university profile content

--- src/profile/UniversityProfile.view.js
+++ src/profile/UniversityProfile.view.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import {
-    Grid
+    Grid,
+    TextField,
+    Tooltip,
+    IconButton
 } from '@material-ui/core';
+
+import {
+    VerifiedUser,
+    MoreHoriz
+} from '@material-ui/icons';
+
+import { Trans } from 'react-i18next';
 
 import { fetchMyProfile, setMyProfile } from './Profile.actions';
 import './Profile.scss';
@@ -20,19 +30,74 @@ class UniversityProfileView extends Component {
 
         return (
             <Grid container justify="center" alignItems="flex-start">
-                <Grid item xs={12} md={6} className="feed-content">
-                    <div className="profile-welcome">
-                        <div className="profile-avatar">
-                            <img
-                                className="profile-avatar-image"
-                                src={account.avatar || `${process.env.PUBLIC_URL}/avatar-default.png`}
-                                alt="User avatar"
-                            />
-
-                            <p className="profile-avatar-text">
-                                {profile.name}
-                            </p>
+                <Grid item xs={12} md={6}>
+                    <div className="profile-header">
+                        <div className="profile-header-actions">
+                            <Grid container justify="flex-end">
+                                <Grid item>
+                                    <Tooltip
+                                        title={<Trans>global.more</Trans>}
+                                        placement="left">
+                                        <IconButton>
+                                            <MoreHoriz />
+                                        </IconButton>
+                                    </Tooltip>
+                                </Grid>
+                            </Grid>
                         </div>
+
+                        <Grid container spacing={16} alignItems="center">
+                            <Grid item>
+                                <div className="profile-avatar">
+                                    <div className={`profile-status ${profile.verified ? 'profile-status-verified' : ''}`}>
+                                        <Tooltip
+                                            placement="right"
+                                            title={profile.verified ? 'Verified' : 'Unverified'}>
+                                            <VerifiedUser />
+                                        </Tooltip>
+                                    </div>
+
+                                    <img
+                                        className="profile-avatar-image"
+                                        src={account.avatar || `${process.env.PUBLIC_URL}/avatar-default.png`}
+                                        alt="User avatar"
+                                    />
+
+                                    <p className="profile-avatar-text">
+                                        {profile.firstName}
+                                    </p>
+                                </div>
+                            </Grid>
+
+                            <Grid item xs={true}>
+                                <Grid container alignItems="center">
+                                    <Grid item xs={true}>
+                                        <TextField
+                                            name="summary"
+                                            value={profile.summary || ''}
+                                            multiline
+                                            fullWidth
+                                            rowsMax="2"
+                                        // onChange={handleProfileUpdate}
+                                        />
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </div>
+
+                    <div className="profile-row">
+                        <Grid container spacing={16}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    label={<Trans>university.name.label</Trans>}
+                                    name="name"
+                                    value={profile.name || ''}
+                                    fullWidth
+                                    // onChange={handleProfileUpdate}
+                                />
+                            </Grid>
+                        </Grid>
                     </div>
                 </Grid>
             </Grid>
