cc028d123c7dcb3a0e9b90488e4d93e02abb321f
Dimitar <mailto.dimitrov@gmail.com>
2019-05-05 23:10:41 +0300

    Extend Partner profile view

--- src/profile/PartnerProfile.view.js
+++ src/profile/PartnerProfile.view.js
@@ -3,37 +3,309 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 import {
-    Grid
+    Grid,
+    TextField,
+    Tooltip,
+    IconButton,
+    InputAdornment,
+    Typography
 } from '@material-ui/core';
 
-import { fetchMyProfile, setMyProfile } from './Profile.actions';
+import {
+    Edit,
+    Save,
+    Cancel
+} from '@material-ui/icons';
+
+import { Trans } from 'react-i18next';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import {
+    fetchMyProfile,
+    setMyProfile,
+    updateMyProfile,
+    updateMyProfileAvatar,
+    setMyProfileData,
+    toggleMyProfileReadonly
+} from './Profile.actions';
+
 import './Profile.scss';
+import { selectProfile } from './Profile.selector';
 
 class PartnerProfile extends Component {
     constructor(props) {
         super(props);
 
-        this.props.fetchMyProfile();
+        this.props.getMyProfile();
     }
 
     render() {
-        const { profile, account } = this.props;
+        const {
+            profile,
+            changeProfileReadonly,
+            handleProfileChange,
+            handleProfileAvatarChange,
+            updateMyProfile
+        } = this.props;
 
         return (
             <Grid container justify="center" alignItems="flex-start">
-                <Grid item xs={12} md={6} className="feed-content">
-                    <div className="profile-welcome">
-                        <div className="profile-avatar">
-                            <img
-                                className="profile-avatar-image"
-                                src={account.avatar || `${process.env.PUBLIC_URL}/avatar-default.png`}
-                                alt="User avatar"
-                            />
+                <Grid item xs={12} md={5}>
+                    {/* PROFILE EDITABLE */}
+                    <div className={`profile-header ${profile.isReadonly ? 'readonly' : ''}`}>
+                        {/* PROFILE HEADER ACTIONS */}
+                        <div className="profile-header-actions">
+                            <Grid container justify="flex-end">
+                                {
+                                    profile.isReadonly ? (
+                                        <Grid item>
+                                            <Tooltip title={<Trans>global.edit</Trans>} placement="left">
+                                                <IconButton onClick={changeProfileReadonly}>
+                                                    <Edit />
+                                                </IconButton>
+                                            </Tooltip>
+                                        </Grid>
+                                    ) : (
+                                            <React.Fragment>
+                                                <Grid item>
+                                                    <Tooltip title={<Trans>global.save</Trans>} placement="left">
+                                                        <IconButton onClick={() => updateMyProfile(profile)}>
+                                                            <Save />
+                                                        </IconButton>
+                                                    </Tooltip>
+                                                </Grid>
 
-                            <p className="profile-avatar-text">
-                                {profile.name}
-                            </p>
+                                                <Grid item>
+                                                    <Tooltip title={<Trans>global.cancel</Trans>} placement="left">
+                                                        <IconButton onClick={changeProfileReadonly}>
+                                                            <Cancel />
+                                                        </IconButton>
+                                                    </Tooltip>
+                                                </Grid>
+                                            </React.Fragment>
+                                        )
+                                }
+                            </Grid>
                         </div>
+
+                        {/* PROFILE HEADER AVATAR */}
+                        <Grid container spacing={16} alignItems="center">
+                            <Grid item>
+                                <div className="profile-avatar">
+                                    <div className={`profile-avatar-image ${profile.isReadonly ? 'readonly' : ''}`}>
+                                        <label htmlFor="profile-avatar-input" className="profile-avatar-label">
+                                            <Trans>global.edit</Trans>
+                                        </label>
+
+                                        <input
+                                            type="file"
+                                            name="avatar"
+                                            id="profile-avatar-input"
+                                            className="profile-avatar-input"
+                                            disabled={profile.isReadonly}
+                                            onChange={handleProfileAvatarChange}
+                                        />
+
+                                        <img
+                                            src={profile.avatar || `${process.env.PUBLIC_URL}/avatar-default.png`}
+                                            alt="User avatar"
+                                        />
+                                    </div>
+                                </div>
+                            </Grid>
+
+                            <Grid item xs={true}>
+                                <Grid container alignItems="center">
+                                    <Grid item xs={true}>
+                                        <TextField
+                                            name="summary"
+                                            label={<Trans>student.summary.label</Trans>}
+                                            value={profile.summary || ''}
+                                            multiline
+                                            fullWidth
+                                            rowsMax="2"
+                                            InputProps={{ readOnly: profile.isReadonly }}
+                                            onChange={handleProfileChange}
+                                        />
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+
+                        <div className="profile-header-content">
+                            {/* PERSONAL SECTION */}
+                            <div className="page-row">
+                                <Typography className="profile-title" variant="h6">
+                                    <Trans>student.personal.label</Trans>
+                                </Typography>
+                            </div>
+
+                            <div className="page-row">
+                                <Grid container spacing={16}>
+                                    {/* FIRST NAME */}
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            label={<Trans>partner.name.label</Trans>}
+                                            name="name"
+                                            value={profile.name || ''}
+                                            variant="standard"
+                                            InputProps={{ readOnly: profile.isReadonly }}
+                                            fullWidth
+                                            onChange={handleProfileChange}
+                                        />
+                                    </Grid>
+                                </Grid>
+                            </div>
+
+                            {/* SOCIALS SECTION */}
+                            <div className="page-row">
+                                <Typography className="profile-title" variant="h6">
+                                    <Trans>student.socials.label</Trans>
+                                </Typography>
+                            </div>
+
+                            <div className="page-row">
+                                <Grid container>
+                                    {/* LINKEDIN */}
+                                    <Grid item xs={true}>
+                                        <TextField
+                                            label={<Trans>student.linkedin.label</Trans>}
+                                            name="linkedinUrl"
+                                            variant="outlined"
+                                            value={profile.linkedinUrl || ''}
+                                            InputProps={{
+                                                readOnly: profile.isReadonly,
+                                                startAdornment: (
+                                                    <InputAdornment position="start">
+                                                        <FontAwesomeIcon color="#0077b5" size="2x" icon={['fab', 'linkedin']}></FontAwesomeIcon>
+                                                    </InputAdornment>
+                                                )
+                                            }}
+                                            fullWidth
+                                            onChange={handleProfileChange}
+                                        />
+                                    </Grid>
+                                </Grid>
+                            </div>
+
+                            {/* FACEBOOK */}
+                            <div className="page-row">
+                                <Grid container>
+                                    <Grid item xs={true}>
+                                        <TextField
+                                            label={<Trans>student.facebook.label</Trans>}
+                                            name="facebookUrl"
+                                            variant="outlined"
+                                            value={profile.facebookUrl || ''}
+                                            InputProps={{
+                                                readOnly: profile.isReadonly,
+                                                startAdornment: (
+                                                    <InputAdornment position="start">
+                                                        <FontAwesomeIcon color="#3b5998" size="2x" icon={['fab', 'facebook']}></FontAwesomeIcon>
+                                                    </InputAdornment>
+                                                )
+                                            }}
+                                            fullWidth
+                                            onChange={handleProfileChange}
+                                        />
+                                    </Grid>
+                                </Grid>
+                            </div>
+
+                            {/* INSTAGRAM */}
+                            <div className="page-row">
+                                <Grid container>
+                                    <Grid item xs={true}>
+                                        <TextField
+                                            label={<Trans>student.instagram.label</Trans>}
+                                            name="instagramUrl"
+                                            variant="outlined"
+                                            value={profile.instagramUrl || ''}
+                                            InputProps={{
+                                                readOnly: profile.isReadonly,
+                                                startAdornment: (
+                                                    <InputAdornment position="start">
+                                                        <FontAwesomeIcon color="#405DE6" size="2x" icon={['fab', 'instagram']}></FontAwesomeIcon>
+                                                    </InputAdornment>
+                                                )
+                                            }}
+                                            fullWidth
+                                            onChange={handleProfileChange}
+                                        />
+                                    </Grid>
+                                </Grid>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* PROFILE READONLY */}
+                    <div className="page-row">
+                        <Grid container>
+                            <Grid item>
+                                <Typography variant="h5">
+                                    {profile.name}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </div>
+
+                    <div className="page-row">
+                        <Grid container>
+                            <Grid item>
+                                <Tooltip title={profile.linkedinUrl ? 'Visit' : 'Unavailable'} placement="top">
+                                    <div>
+                                        <IconButton
+                                            href={`https://www.linkedin.com/in/${profile.linkedinUrl}/`}
+                                            rel="noopener noreferrer"
+                                            target="_blank"
+                                            disabled={!profile.linkedinUrl}>
+                                            <FontAwesomeIcon
+                                                color={profile.linkedinUrl ? '#0077b5': '#ccc'}
+                                                size="1x"
+                                                icon={['fab', 'linkedin']}>
+                                            </FontAwesomeIcon>
+                                        </IconButton>
+                                    </div>
+                                </Tooltip>
+                            </Grid>
+
+                            <Grid item>
+                                <Tooltip title={profile.facebookUrl ? 'Visit' : 'Unavailable'} placement="top">
+                                    <div>
+                                        <IconButton
+                                            href={`https://www.facebook.com/${profile.facebookUrl}/`}
+                                            rel="noopener noreferrer"
+                                            target="_blank"
+                                            disabled={!profile.facebookUrl}>
+                                            <FontAwesomeIcon
+                                                color={profile.facebookUrl ? '#3b5998': '#ccc'}
+                                                size="1x"
+                                                icon={['fab', 'facebook']}>
+                                            </FontAwesomeIcon>
+                                        </IconButton>
+                                    </div>
+                                </Tooltip>
+                            </Grid>
+
+                            <Grid item>
+                                <Tooltip title={profile.instagramUrl ? 'Visit' : 'Unavailable'} placement="top">
+                                    <div>
+                                        <IconButton
+                                            href={`https://www.instagram.com/${profile.instagramUrl}/`}
+                                            rel="noopener noreferrer"
+                                            target="_blank"
+                                            disabled={!profile.instagramUrl}>
+                                            <FontAwesomeIcon
+                                                color={profile.instagramUrl ? '#405DE6': '#ccc'}
+                                                size="1x"
+                                                icon={['fab', 'instagram']}>
+                                            </FontAwesomeIcon>
+                                        </IconButton>
+                                    </div>
+                                </Tooltip>
+                            </Grid>
+                        </Grid>
                     </div>
                 </Grid>
             </Grid>
@@ -43,14 +315,31 @@ class PartnerProfile extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        profile: state.profile,
-        account: state.account
+        profile: selectProfile(state)
     };
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        fetchMyProfile() {
+        handleProfileAvatarChange(e) {
+            return dispatch(updateMyProfileAvatar(e.target.files[0]))
+                .then((updatedProfile) => {
+                    return dispatch(setMyProfile(updatedProfile));
+                });
+        },
+        handleProfileChange(e) {
+            return dispatch(setMyProfileData(e.target.name, e.target.value));
+        },
+        changeProfileReadonly() {
+            return dispatch(toggleMyProfileReadonly());
+        },
+        updateMyProfile(updates) {
+            return dispatch(updateMyProfile(updates))
+                .then((updatedProfile) => {
+                    return dispatch(setMyProfile(updatedProfile));
+                });
+        },
+        getMyProfile() {
             return dispatch(fetchMyProfile())
                 .then((profileData) => {
                     return dispatch(setMyProfile(profileData));
