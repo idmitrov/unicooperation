4363522a8c95256e04aae063c60442b3d97d011f
Dimitar <mailto.dimitrov@gmail.com>
2019-05-18 22:57:25 +0300

    Add selectAccount

--- src/account/Account.selector.js
+++ src/account/Account.selector.js
@@ -1,10 +1,18 @@
 import { createSelector } from 'reselect';
 
 const getAccountToken = (state) => state.account.token;
+const getAccount = (state) => state.account;
 
 export const selectAccountToken = createSelector(
     [getAccountToken],
     (accountToken) => {
         return accountToken;
+    }
+)
+
+export const selectAccount = createSelector(
+    [getAccount],
+    (account) => {
+        return account;
     }
 )
