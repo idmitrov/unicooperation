fc81d5a21c905fd7c61851bfd06f0634e5c01771
Dimitar <mailto.dimitrov@gmail.com>
2019-06-17 23:06:10 +0300

    Init Matcher searching bar

--- src/matcher/Matcher.view.js
+++ src/matcher/Matcher.view.js
@@ -1,15 +1,106 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { Grid } from '@material-ui/core';
+import {
+    AppBar,
+    Grid,
+    TextField,
+    IconButton,
+    Tooltip
+} from '@material-ui/core';
+
+import {
+    FilterList,
+    Close,
+    Done
+} from '@material-ui/icons';
+
+import { Trans } from 'react-i18next';
+
+import '../app/App.scss';
 
 class MatcherView extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            isInputExpanded: false,
+            searchQuery: ''
+        };
+    }
+
     render() {
         return (
             <Grid container justify="center" alignItems="flex-start">
                 <Grid item xs={12} md={6} lg={4}>
-                    <div className="page-row">
-                        <div>Matcher view</div>
+                    <div>
+                        <AppBar
+                            className="top-bar"
+                            position="sticky">
+                            <form onSubmit={(e) => {
+                                e.preventDefault();
+
+                                // TODO: Apply filter and call API with the filter
+
+                                this.setState({
+                                    ...this.state,
+                                    isInputExpanded: !this.state.isInputExpanded,
+                                });
+                            }}>
+                                <div className={`bar-input ${this.state.isInputExpanded ? 'expanded' : ''}`}>
+                                    <div className="bar-input-inner">
+                                        <Grid container alignItems="center">
+                                            <Grid item xs={12}>
+                                                Search filter inputs
+                                            </Grid>
+                                        </Grid>
+                                    </div>
+                                </div>
+
+                                <Grid container justify="space-between" alignItems="center">
+                                    <Grid item xs={true}>
+                                        {
+                                            this.state.isInputExpanded ? (
+                                                <div>
+                                                    <Tooltip title={<Trans>global.cancel</Trans>} placement="right">
+                                                        <IconButton
+                                                            type="button"
+                                                            onClick={() => this.setState({ ...this.state, isInputExpanded: false })}>
+                                                            <Close />
+                                                        </IconButton>
+                                                    </Tooltip>
+                                                </div>
+                                            ) : (
+                                                <TextField
+                                                    label="Title"
+                                                    value={this.state.searchQuery}
+                                                    fullWidth
+                                                    onChange={(e) => this.setState({ ...this.state, searchQuery: e.target.value })}
+                                                />
+                                            )
+                                        }
+                                    </Grid>
+
+                                    <Grid item>
+                                        {
+                                            this.state.isInputExpanded ? (
+                                                <Tooltip title={<Trans>global.apply</Trans>} placement="left">
+                                                    <IconButton type="button" onClick={() => this.setState({ ...this.state, isInputExpanded: false, searchQuery: '' })}>
+                                                        <Done />
+                                                    </IconButton>
+                                                </Tooltip>
+                                            ) : (
+                                                <Tooltip title={<Trans>global.filter</Trans>} placement="left">
+                                                    <IconButton type="button" onClick={() => this.setState({ ...this.state, isInputExpanded: true, searchQuery: '' })}>
+                                                        <FilterList />
+                                                    </IconButton>
+                                                </Tooltip>
+                                            )
+                                        }
+                                    </Grid>
+                                </Grid>
+                            </form>
+                        </AppBar>
                     </div>
                 </Grid>
             </Grid>
