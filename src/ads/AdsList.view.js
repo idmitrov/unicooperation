96382a72c3e6590cb2407b0898acd7f82276ad53
Dimitar <mailto.dimitrov@gmail.com>
2019-07-01 21:14:37 +0300

    Tweak ads list

--- src/ads/AdsList.view.js
+++ src/ads/AdsList.view.js
@@ -4,23 +4,25 @@ import { Link } from 'react-router-dom';
 import { Trans } from 'react-i18next';
 
 import {
-    Button,
-    Card,
-    CardHeader,
-    CardActions,
     Grid,
-    Typography
+    IconButton,
+    Tooltip
 } from '@material-ui/core';
+
+import {
+    Visibility,
+    Edit
+} from '@material-ui/icons';
 
 import '../app/App.scss';
 
 import {
     fetchMyads,
     fetchMyUniversityPartnersAds,
-    setAdsList,
-    applyToAd,
-    updateAdsList
+    setAdsList
 } from './Ads.actions';
+
+import ProfileIntroCard from '../components/profile-intro-card/ProfileIntroCard.component';
 
 import { accountType } from '../account/Account.constants';
 
@@ -30,71 +32,69 @@ class AdsListView extends Component {
     constructor(props) {
         super(props);
 
-        switch (this.props.loggedAccountType) {
+        switch (this.props.loggedInAccount.type) {
             case accountType.student:
                 this.props.fetchMyUniversityPartnersAds();
                 break;
             case accountType.partner:
                 this.props.fetchMyads();
                 break;
-            default: console.error('Unknown accountType');
+            default: console.error('Fetch Ads -> Unknown accountType');
         }
     }
 
     render() {
         const {
             ads,
-            loggedAccountType,
-            applyToAd
+            loggedInAccount
         } = this.props;
 
         return (
             <Grid container justify="center" alignItems="flex-start">
                 <Grid item xs={12} md={6} lg={4}>
-                    <div className="page-row">
+                    <div className="ads-grid pages-row">
                         <Grid container spacing={16}>
                             {
                                 ads.map((adItem, index) => {
                                     return (
                                         <Grid item xs={12} sm={6} key={index}>
-                                            <Card>
-                                                <CardHeader title={
-                                                    <Typography variant="h6">
+                                            <ProfileIntroCard
+                                                avatar={adItem.conver}
+                                                title={
+                                                    adItem.title.length < 17
+                                                        ? adItem.title
+                                                        : `${adItem.title.substring(0, 17)}...`
+                                                }
+                                                hoverText={
+                                                    <Trans>ads.list.item.intro</Trans>
+                                                }
+                                                actions={
+                                                    <React.Fragment>
                                                         {
-                                                            adItem.title.length < 17
-                                                                ? adItem.title
-                                                                : `${adItem.title.substring(0, 17)}...`
+                                                            loggedInAccount.type === accountType.partner &&
+                                                            loggedInAccount.profile === adItem.author ? (
+                                                                <Link to={`/ads/edit/${adItem._id}`}>
+                                                                    <Tooltip title={<Trans>ads.list.item.edit</Trans>}>
+                                                                        <IconButton className="ad-icon-button">
+                                                                            <Edit className="ad-icon" />
+                                                                        </IconButton>
+                                                                    </Tooltip>
+                                                                </Link>
+                                                            ) :(
+                                                                <Link to={`/ads/details/${adItem._id}`}>
+                                                                    <Tooltip title={<Trans>ads.list.item.details</Trans>}>
+                                                                        <IconButton className="ad-icon-button">
+                                                                            <Visibility className="ad-icon" />
+                                                                        </IconButton>
+                                                                    </Tooltip>
+                                                                </Link>
+                                                            )
                                                         }
-                                                    </Typography>
-                                                } />
-
-                                                <CardActions>
-                                                    <Button fullWidth>
-                                                        <Link to={`/ads/details/${adItem._id}`} className="ad-anchor-button">
-                                                            <Trans>ads.list.item.details</Trans>
-                                                        </Link>
-                                                    </Button>
-
-                                                    {
-                                                        loggedAccountType === accountType.student ? (
-                                                            <Button
-                                                                disabled={adItem.applied}
-                                                                fullWidth
-                                                                onClick={() => applyToAd(adItem)}>
-                                                                <Trans>{adItem.applied ? 'ads.list.item.applied' : 'ads.list.item.apply' }</Trans>
-                                                            </Button>
-                                                        ) : loggedAccountType === accountType.partner ? (
-                                                            <Button
-                                                                fullWidth>
-                                                                <Trans>ads.list.item.edit</Trans>
-                                                            </Button>
-                                                        ) : (null)
-                                                    }
-                                                </CardActions>
-
-                                            </Card>
+                                                    </React.Fragment>
+                                                }
+                                            />
                                         </Grid>
-                                    );
+                                    )
                                 })
                             }
                         </Grid>
@@ -107,21 +107,13 @@ class AdsListView extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        loggedAccountType: state.account.type,
+        loggedInAccount: state.account,
         ads: state.ads.list
     };
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        applyToAd(adToApply) {
-            if (!adToApply.applied) {
-                return dispatch(applyToAd(adToApply._id))
-                    .then((ad) => {
-                        return dispatch(updateAdsList(ad));
-                    });
-            }
-        },
         fetchMyUniversityPartnersAds() {
             return dispatch(fetchMyUniversityPartnersAds())
                 .then((ads) => {
