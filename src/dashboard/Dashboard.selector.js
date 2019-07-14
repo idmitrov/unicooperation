da8ccc98b11ead231def0e4bda78c7b5bef4b508
Dimitar <mailto.dimitrov@gmail.com>
2019-07-14 14:30:12 +0300

    Init Dashboard selectors

---
+++ src/dashboard/Dashboard.selector.js
@@ -0,0 +1,22 @@
+import { createSelector } from 'reselect';
+
+const MAX_VISIBLE_ITEMS = 2;
+
+const getInterviews = (state) => state.interview.list;
+const getAds = (state) => state.ads.list;
+const getMatches = (state) => state.matcher.matches;
+
+export const selectTopInterviews = createSelector(
+    [getInterviews],
+    (interviews) => interviews.slice(0, MAX_VISIBLE_ITEMS)
+);
+
+export const selectTopAds = createSelector(
+    [getAds],
+    (ads) => ads.slice(0, MAX_VISIBLE_ITEMS)
+)
+
+export const selectoTopMatches = createSelector(
+    [getMatches],
+    (matches) => matches.slice(0, MAX_VISIBLE_ITEMS)
+)
