ca5603316ac4b8ecf61d156aba8f44763f61264f
Dimitar <mailto.dimitrov@gmail.com>
2019-07-19 23:42:54 +0300

    Add Bulgarian test translations and lang detect plugin to i18n

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,17 +1,20 @@
 import i18n from "i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from "react-i18next";
 import resources from '../locales.json';
 
-i18n
+export default i18n
+    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
-        resources,
-        lng: 'en',
+        resources: resources,
         keySeparator: false,
-        fallbackLng: 'en',
+        fallbackLng: 'en-US',
         interpolation: {
             escapeValue: false
+        },
+        order: ['localStorage'],
+        detection: {
+            lookupLocalStorage: 'uni-lang'
         }
     });
-
-export default i18n;
